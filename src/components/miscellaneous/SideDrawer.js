48f0def0c397c86596ff7c1ae6892c27b2af51f8
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-03 01:49:18 +0530

    Update SideDrawer.js

--- src/components/miscellaneous/SideDrawer.js
+++ src/components/miscellaneous/SideDrawer.js
@@ -81,7 +81,7 @@ function SideDrawer() {
         },
       };
 
-      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      const { data } = await axios.get(`https://connectusbackend.onrender.com/api/user?search=${search}`, config);
 
       setLoading(false);
       setSearchResult(data);
@@ -109,7 +109,7 @@ function SideDrawer() {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.post(`/api/chat`, { userId }, config);
+      const { data } = await axios.post(`https://connectusbackend.onrender.com/api/chat`, { userId }, config);
 
       if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
       setSelectedChat(data);
